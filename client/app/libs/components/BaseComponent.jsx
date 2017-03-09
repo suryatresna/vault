78d2b72995040f89d4b5b52b13e1db1e544c7232
Surya Tresna <surya.tresna@go-jek.com>
2017-03-10 00:01:13 +0700

    Update

--- client/app/libs/components/BaseComponent.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import PureRenderMixin from 'react-addons-pure-render-mixin';
-
-export default class BaseComponent extends React.Component {
-  shouldComponentUpdate(theArgs) {
-    return PureRenderMixin.shouldComponentUpdate.apply(this, ...theArgs);
-  }
-}
